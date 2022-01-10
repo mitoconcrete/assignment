6d83a3d7f25cb5834c967eea919e67f6b0aae25d
Taehoon Kim <xogns98@gmail.com>
2022-01-10 20:38:48 +0900

    add] article item function add into app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import Navigation from "./components/Navigation.js";
+import ArticleItems from "./components/ArticleItems.js";
 
 const menus = [
-  { href: "introduce", label: "introduce" },
-  { href: "product", label: "product" },
-  { href: "contact", label: "contact" },
+  { href: "introduce", label: "제품소개" },
+  { href: "product", label: "주요기능" },
+  { href: "contact", label: "고객지원" },
 ];
 
 const navigation = new Navigation(menus);
 navigation.init();
+
+const articleItems = new ArticleItems();
+articleItems.init();
